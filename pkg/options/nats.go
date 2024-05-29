19e69e3e0d1f382b5638bdfd58bfbf89651fff4a
Danielius Dvareckas <danielius@noia.network>
2024-05-29 12:46:23 +0300

    fix: typo

--- pkg/options/nats.go
+++ pkg/options/nats.go
@@ -12,7 +12,7 @@ import (
 // MakeNats is a convenience function to create NATS connection from various parameters.
 func MakeNats(name, urls, userCreds, nkey, jwt, caCert, clientCert, clientKey string) (*nats.Conn, error) {
 	if urls == "" {
-		urls := strings.Join(config.DefaultNetworks.GetDefault().URLs, ",")
+		urls = strings.Join(config.DefaultNetworks.GetDefault().URLs, ",")
 		if urls == "" {
 			slog.Info("Will use NATS stub")
 			return nil, nil
